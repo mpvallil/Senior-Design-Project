28fce95ba79947a7d2976068ecc71f1c25c0e315
mpvallil <mpvallil@ncsu.edu>
2018-10-16 23:20:54 -0400

    test commit

--- app/src/main/java/ncsu/project15/ece484_project15_client/MainMenu.java
+++ app/src/main/java/ncsu/project15/ece484_project15_client/MainMenu.java
@@ -1,7 +1,10 @@
 package ncsu.project15.ece484_project15_client;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Button test_maps_button = findViewById(R.id.test_map_layout_button);
+
+        test_maps_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenu.this, MapsActivity.class));
+            }
+        });
     }
 }
